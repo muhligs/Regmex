a707162d65ff2afdf7939b84cf9e355d2bb3c67c
Morten Muhlig <muhligs@gmail.com>
2014-09-24 12:07:54 +0200

    commit14

--- R/motifs.seqs.R
+++ R/motifs.seqs.R
@@ -1,6 +1,11 @@
 motifs.seqs <-
 function(seqs,k){ # include
+if(class(seqs)=="seq"){seqs <- seqs$sequence}
+if(class(seqs)=="list"){
+  if(!(class(seqs[[1]]) %in% c("character","seq"))){stop("seq should be a 'seq' object or a string or a list of 'seq' objects or strings")}
+ 	if(class(seqs[[1]])=="seq"){seqs <- lapply(seqs,function(x)x$sequence)}
+}
 vec <- c()
-for(i in seqs)vec <- c(vec,mot.chop(i,k,unique=TRUE))
+for(i in seqs) {vec <- c(vec,mot.chop(i,k,unique=TRUE))}
 return(unique(vec))
 }
