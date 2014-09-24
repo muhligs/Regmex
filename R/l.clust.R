26d5ee450268d7f37444f2b544ab027108d7ce9f
Morten Muhlig <muhligs@gmail.com>
2014-09-24 14:28:30 +0200

    commit17

--- R/l.clust.R
+++ R/l.clust.R
@@ -1,6 +1,8 @@
 l.clust <-
 function(set,k,plot=TRUE,method="average"){ # include
 d  <- adist(set)
+	rownames(d) <- set
+	print(class(dist))
 hc <- hclust(as.dist(d),method=method)
 	c.ex <- ifelse(length(set)<70,30/length(set),40/length(set))
 if(plot==TRUE)	plot(hc,xlab = "motifs",cex=c.ex)
