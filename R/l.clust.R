571d59f48d824ea822ac6b927a303b0454efc29e
Morten Muhlig <muhligs@gmail.com>
2014-09-24 14:12:19 +0200

    commit16

--- R/l.clust.R
+++ R/l.clust.R
@@ -1,10 +1,13 @@
 l.clust <-
 function(set,k,plot=TRUE,method="average"){ # include
-d  <- adist(set)
-rownames(d) <- set
+d  <- adist(as.character(set))
+rownames(d) <- as.character(set)
 hc <- hclust(as.dist(d),method=method)
 	c.ex <- ifelse(length(set)<70,30/length(set),40/length(set))
 if(plot==TRUE)	plot(hc,xlab = "motifs",cex=c.ex)
 rect.hclust(hc,k=k)
-return(data.frame(cutree(hc,k=k)))
+res <- 	data.frame(cutree(hc,k=k))
+rownames(res) <- 	as.character(set)
+	colnames(res)[2] <- "cluster"
+return()
 }
