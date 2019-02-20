140a40abd169dcbd981fb70b5faa581fa4708e19
muhligs <muhligs@users.noreply.github.com>
2019-02-20 11:40:21 +0100

    added variance
    and subtracted 1 from result. Countered in bb.motif.p

--- R/bb.cumdist.R
+++ R/bb.cumdist.R
@@ -1,2 +1,2 @@
 bb.cumdist <-
-function(m,n){return(ifelse(m<0.01,0,abs(1-2*sum((-1)^(0:999)*exp(-2/n*((1:1000)^2)*m^2)))))}
+function(m,n,varls){return(ifelse(m<0.01,1,abs(2*sum((-1)^(0:999)*exp(-2/n/varls*((1:1000)^2)*m^2)))))}
